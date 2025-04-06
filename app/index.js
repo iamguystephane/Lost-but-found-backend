f3ada9321946f58b9e5488cb96baddab66faf117
iamguystephane <gstephane138@gmail.com>
2025-04-06 21:55:41 +0100

    installed express, and created docker container files for deployment

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const app = require("express");
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1> Hello world, welcome to our lost but found app backend! </h1>"
+  );
+});
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000 at http://localhost:5000");
+});
