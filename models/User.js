a02b8da4d95b7b4470614c5dc995f415252f15d3
iamguystephane <gstephane138@gmail.com>
2025-05-03 00:38:30 +0100

    Connected to the database and created basic API routes

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userScheme = new mongoose.Schema({
+  names: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userScheme);
+module.exports = User;
